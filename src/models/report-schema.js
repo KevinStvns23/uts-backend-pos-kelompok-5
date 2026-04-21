107e5820a764a0164a693b27db25bf3cc48b074d
KevinStvns23 <stevkevin22@gmail.com>
2026-04-21 19:25:41 +0700

    Rename productSchema to reportSchema

--- src/models/report-schema.js
+++ src/models/report-schema.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+const reportSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String },
     stock: { type: Number, required: true, default: 0 }
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('Product', reportSchema);
